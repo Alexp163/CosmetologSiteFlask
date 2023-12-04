655255da3fe0151f6467a94963dda77d21a4fa7e
Alexp163 <dr.p163@ya.ru>
2023-12-04 23:06:55 +0400

    1. Добавить юзера в модели и в админ панель(admin.py, model.py, db.py) 2. выполнить миграции 3. Зайти в админ-панель, создать запись

--- src/admin.py
+++ src/admin.py
@@ -1,7 +1,7 @@
 from flask_admin import Admin
 from app import app
 from flask_admin.contrib.sqla import ModelView
-from models import CosmetologyService, Service, ServiceGroup
+from models import CosmetologyService, Service, ServiceGroup, User
 from db import db
 
 
@@ -18,7 +18,13 @@ class ServiceModelView(ModelView):
     form_excluded_columns = ('created_at', 'updated_at')
 
 
+class UserModelView(ModelView):
+    column_list = ('nick_name', 'email')
+    form_excluded_columns=('created_at', 'updated_at', 'password_hash')
+
+
 admin.add_view(CosmetologyServiceModelView(CosmetologyService, db.session))
 admin.add_view(ServiceModelView(Service, db.session))
 admin.add_view(ModelView(ServiceGroup, db.session))
+admin.add_view(UserModelView(User, db.session))
 
